10cccbb7151f1c9fc2905e0c0f4531ae4439f424
tyler suderman and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-04-21 14:34:28 -0700

    add dinner menu

--- app/src/main/java/com/example/guest/thedump/DinnerActivity.java
+++ app/src/main/java/com/example/guest/thedump/DinnerActivity.java
@@ -2,12 +2,23 @@ package com.example.guest.thedump;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class DinnerActivity extends AppCompatActivity {
+    @Bind(R.id.dinnerListView)
+    ListView mDinnerListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dinner);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.dinner_array, android.R.layout.simple_list_item_1);
+        mDinnerListView.setAdapter(adapter);
     }
 }
