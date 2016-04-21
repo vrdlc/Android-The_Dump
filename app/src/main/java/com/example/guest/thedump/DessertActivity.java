073a064a7d6dc70bf04406f39809ef286891280b
tyler suderman and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-04-21 14:37:47 -0700

    add dessert menu

--- app/src/main/java/com/example/guest/thedump/DessertActivity.java
+++ app/src/main/java/com/example/guest/thedump/DessertActivity.java
@@ -2,12 +2,23 @@ package com.example.guest.thedump;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class DessertActivity extends AppCompatActivity {
+    @Bind(R.id.dessertListView)
+    ListView mDessertListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dessert);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.dessert_array, android.R.layout.simple_list_item_1);
+        mDessertListView.setAdapter(adapter);
     }
 }
