99295136dd85361a60e9a6e3f0ecf61ec8c91151
tyler suderman and Ramon de la Cruz <tcsuder@gmail.com>
2016-04-21 14:14:14 -0700

    add breakfast menu list

--- app/src/main/java/com/example/guest/thedump/BreakfastActivity.java
+++ app/src/main/java/com/example/guest/thedump/BreakfastActivity.java
@@ -2,12 +2,25 @@ package com.example.guest.thedump;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class BreakfastActivity extends AppCompatActivity {
+    private ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breakfast);
+
+        mListView = (ListView) findViewById(R.id.breakfastListView);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.breakfast_array, android.R.layout.simple_list_item_1);
+        mListView.setAdapter(adapter);
+
+
+            
     }
 }
